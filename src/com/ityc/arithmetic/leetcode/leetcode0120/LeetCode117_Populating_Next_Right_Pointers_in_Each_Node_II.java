cfa307b5acc05910c9e76e3ca5ac24354fdf1d5d
yuchao <2994413619@qq.com>
2023-11-17 00:10:31 +0800

    feat:添加leetcode第117题——二叉树的层级遍历

--- src/com/ityc/arithmetic/leetcode/leetcode0120/LeetCode117_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ src/com/ityc/arithmetic/leetcode/leetcode0120/LeetCode117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -6,17 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 填充每个节点的下一个右侧节点指针 ac:100%
+ * 填充每个节点的下一个右侧节点指针 II ac:100%
  * @author yuchao
- * @Date 2023/11/16 22:05
+ * @Date 2023/11/17 00:09
  **/
-public class LeetCode116_Populating_Next_Right_Pointers_in_Each_Node {
+public class LeetCode117_Populating_Next_Right_Pointers_in_Each_Node_II {
 
     public Node connect(Node root) {
         List<Node> nodes = new ArrayList<>();
         if (root != null) {
-            if (root.right != null) {
+            if (root.left != null) {
                 nodes.add(root.left);
+            }
+            if (root.right != null) {
                 nodes.add(root.right);
             }
         }
@@ -24,8 +26,10 @@ public class LeetCode116_Populating_Next_Right_Pointers_in_Each_Node {
         while (nodes.size() != 0) {
             List<Node> temp = new ArrayList<>();
             for(int i = 0; i < nodes.size(); i++) {
-                if(nodes.get(i).right != null) {
+                if (nodes.get(i).left != null) {
                     temp.add(nodes.get(i).left);
+                }
+                if (nodes.get(i).right != null) {
                     temp.add(nodes.get(i).right);
                 }
                 if (i < nodes.size() - 1) {
