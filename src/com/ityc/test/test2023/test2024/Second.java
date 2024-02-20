81c38cbecb2481314ba97bcf50d18f41d11cfdf4
yuchao <2994413619@qq.com>
2024-02-20 19:35:13 +0800

    feat:添加测试随机数的测试代码

---
+++ src/com/ityc/test/test2023/test2024/Second.java
@@ -0,0 +1,16 @@
+package com.ityc.test.test2023.test2024;
+
+/**
+ *
+ * @author yuchao
+ * @date 2024/2/20 10:29
+ **/
+public class Second {
+
+    public static void main(String[] args) {
+        Integer i = null;
+        System.out.println(String.valueOf(i));
+        System.out.println(i == 1);
+    }
+
+}
