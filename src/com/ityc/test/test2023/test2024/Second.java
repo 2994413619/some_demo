b8229816bf7399600af7eca5fed6050127f0fd80
yuchao <2994413619@qq.com>
2024-02-22 14:33:41 +0800

    feat:添加Integer类型使用==的测试代码

--- src/com/ityc/test/test2023/test2024/Second.java
+++ src/com/ityc/test/test2023/test2024/Second.java
@@ -8,9 +8,18 @@ package com.ityc.test.test2023.test2024;
 public class Second {
 
     public static void main(String[] args) {
-        Integer i = null;
+        /*Integer i = null;
         System.out.println(String.valueOf(i));
-        System.out.println(i == 1);
+        System.out.println(i == 1);*/
+        /**
+         * 测试Integer类型==和equals，==只会在-128——127之间有效
+         */
+        Integer a = 199, b = 199;
+        System.out.println(a == b);
+        System.out.println(a.equals(199));
+        System.out.println(a.equals(null));
+        Integer c = 1;
+        System.out.println(c == 1);
     }
 
 }
